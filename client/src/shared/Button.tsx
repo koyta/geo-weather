ed6defc42d8668ec15f90195ad6b4c0946098750
Andrey Khlinvyuk <koytaboyta@gmail.com>
2019-02-11 14:14:27 +0300

    Move and rename components

---
+++ client/src/shared/Button.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+import { Theme } from "../theme";
+import { StyledButton } from "./Button/StyledButton";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLElement> {
+  onClick?: (e: React.SyntheticEvent) => void;
+  label: string;
+  theme?: Theme;
+  primary?: boolean;
+}
+
+class Button extends React.Component<ButtonProps, {}> {
+  handleClick = (e: React.SyntheticEvent) => {
+    if (typeof this.props.onClick === "function") {
+      this.props.onClick(e);
+    }
+  };
+
+  render() {
+    const { theme, label, ...rest } = this.props;
+    return (
+      <StyledButton onClick={this.handleClick} theme={theme} {...rest}>
+        {label}
+      </StyledButton>
+    );
+  }
+}
+
+export default Button;
